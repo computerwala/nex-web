bb34e515d803c75e8183a3e16b21d51f3dc2b610
computerwala <30777038+computerwala@users.noreply.github.com>
2018-09-04 02:11:47 +0530

    Add files via upload

---
+++ nixx.js
@@ -0,0 +1,10 @@
+  	$("#typed").typed({
+  		strings: ["Hello, my name is Herbarium!"],
+  		typeSpeed: 90,
+  		startDelay: 0,
+  		backSpeed: 30,
+  		backDelay: 500,
+  		loop: true,
+  		cursorChar: "|",
+  		contentType: 'html'
+  	});
